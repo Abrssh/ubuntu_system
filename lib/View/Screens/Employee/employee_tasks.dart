44fbf059ef13bdb59c289e9c4282bea982305463
Abrham <65964297+Abrssh@users.noreply.github.com>
2024-12-01 20:50:47 +0300

    Future builder implementation

--- lib/View/Screens/Employee/employee_tasks.dart
+++ lib/View/Screens/Employee/employee_tasks.dart
@@ -98,21 +98,67 @@ class _EmployeeTasksState extends State<EmployeeTasks> {
   late List<Task> tasks = [];
   bool loading = false;
 
-  @override
-  void initState() {
-    super.initState();
-    var empProv = context.read<EmployeeAccProvider>();
-    TaskDatabaseService()
-        .getTasksForEmployee(empProv.employeeAccVal!.employeeDocId)
-        .then((value) {
-      tasks.addAll(value);
-      setState(() {
-        loading = true;
-      });
-    });
-  }
+  //previous code
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   var empProv = context.read<EmployeeAccProvider>();
+  //   TaskDatabaseService()
+  //       .getTasksForEmployee(empProv.employeeAccVal!.employeeDocId)
+  //       .then((value) {
+  //     tasks.addAll(value);
+  //     setState(() {
+  //       loading = true;
+  //     });
+  //   });
+  // }
 
   DateFormat dateFormat = DateFormat("yMd");
+
+  // previous code
+  // @override
+  // Widget build(BuildContext context) {
+  //   final double deviceHeight = MediaQuery.of(context).size.height;
+  //   final double deviceWidth = MediaQuery.of(context).size.width;
+  //   const isWeb = kIsWeb; // Determine if running on web
+
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: const Text('Tasks Done'),
+  //       centerTitle: true,
+  //     ),
+  //     body: loading
+  //         ? SizedBox(
+  //             height: deviceHeight * 0.9,
+  //             child: ListView.builder(
+  //               itemCount: tasks.length,
+  //               itemBuilder: (context, index) {
+  //                 final task = tasks[index];
+  //                 return Card(
+  //                   elevation: deviceWidth * 0.04,
+  //                   child: ListTile(
+  //                     title: Text('Task ID: ${task.outlierTaskId}'),
+  //                     subtitle: Column(
+  //                       crossAxisAlignment: CrossAxisAlignment.start,
+  //                       children: [
+  //                         Text(
+  //                             'Amount Earned: \$${task.payAmount.toStringAsFixed(2)}'),
+  //                         Text(
+  //                             'Created Date: ${dateFormat.format(task.createdDate)}'),
+  //                         Text(
+  //                             'Feedback Date: ${dateFormat.format(task.feedbackDate)}'),
+  //                         Text(
+  //                             'Feedback: ${task.feedback == 99 ? 'N/A' : task.feedback}'),
+  //                       ],
+  //                     ),
+  //                   ),
+  //                 );
+  //               },
+  //             ),
+  //           )
+  //         : const LoadingAnimation2(),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -125,8 +171,19 @@ class _EmployeeTasksState extends State<EmployeeTasks> {
         title: const Text('Tasks Done'),
         centerTitle: true,
       ),
-      body: loading
-          ? SizedBox(
+      body: FutureBuilder<List<Task>>(
+        future: TaskDatabaseService().getTasksForEmployee(
+            context.read<EmployeeAccProvider>().employeeAccVal!.employeeDocId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingAnimation2();
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No tasks available'));
+          } else {
+            final tasks = snapshot.data!;
+            return SizedBox(
               height: deviceHeight *
                   (isWeb ? 1.0 : 0.9), // Adjust height based on platform
               child: ListView.builder(
@@ -160,8 +217,10 @@ class _EmployeeTasksState extends State<EmployeeTasks> {
                   );
                 },
               ),
-            )
-          : const LoadingAnimation2(),
+            );
+          }
+        },
+      ),
     );
   }
 }
